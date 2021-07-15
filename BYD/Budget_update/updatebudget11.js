5bccb5e9d0fe684905b9ac06fda02f8f426e1da0
moioo91120 <moioo91@gmail.com>
2021-07-15 20:02:15 +0200

    new

--- BYD/Budget_update/updatebudget11.js
+++ BYD/Budget_update/updatebudget11.js
@@ -163,21 +163,21 @@
         var that_ = that;
         var restAPIURL = "https://www.google.com";
 		console.log("in UI5");
-
+		
         if (that._firstConnectionUI5 === 0) {
             console.log("--First Time --");
 
             let div0 = document.createElement('div');
             div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View xmlns="sap.m" xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" xmlns:l="sap.ui.layout" height="100%" controllerName="myView.Template"><l:VerticalLayout class="sapUiContentPadding" width="100%"><l:content><Input id="input"  placeholder="Enter partner number..." liveChange=""/></l:content><Button id="buttonId" class="sapUiSmallMarginBottom" text="Get Score" width="150px" press=".onButtonPress" /></l:VerticalLayout></mvc:View></script>';
-            _shadowRoot.appendChild(div0);
+            that_._shadowRoot.appendChild(div0);
 
             let div1 = document.createElement('div');
             div1.innerHTML = '<div id="ui5_content_' + widgetName + '" name="ui5_content_' + widgetName + '"><slot name="content_' + widgetName + '"></slot></div>';
-            _shadowRoot.appendChild(div1);
+            that_._shadowRoot.appendChild(div1);
 
             that_.appendChild(div);
 
-            var mapcanvas_divstr = _shadowRoot.getElementById('oView_' + widgetName);
+            var mapcanvas_divstr = that_._shadowRoot.getElementById('oView_' + widgetName);
 
             Ar.push({
                 'id': widgetName,
